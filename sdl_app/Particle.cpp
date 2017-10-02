842060b62dc45c72df77af71d09ab7b07fc4d45a
Slava Bezgachev <bezgachev@gmail.com>
2017-10-02 14:53:53 +0600

    Particles now bounce off the edges of the screen, move wit ha static random speed

--- sdl_app/Particle.cpp
+++ sdl_app/Particle.cpp
@@ -8,6 +8,9 @@
 Particle::Particle() {
     m_x = ((2.0 * rand()) / RAND_MAX) - 1;
     m_y = ((2.0 * rand()) / RAND_MAX) - 1;
+
+    m_xspeed = 0.001 * ((2.0 * rand()/RAND_MAX) - 1);
+    m_yspeed = 0.001 * ((2.0 * rand()/RAND_MAX) - 1);
 }
 
 Particle::~Particle() {
@@ -15,9 +18,16 @@ Particle::~Particle() {
 }
 
 void Particle::update() {
-    const double xspeed = 0.001 * rand()/RAND_MAX;
-    const double yspeed = 0.001 * rand()/RAND_MAX;
 
-    m_x += xspeed;
-    m_y += yspeed;
+    m_x += m_xspeed;
+    m_y += m_yspeed;
+
+    if (m_x < -1.0 || m_x >= 1.0){
+        m_xspeed = -m_xspeed;
+    }
+
+    if (m_y < -1.0 || m_y >= 1.0){
+        m_yspeed = -m_yspeed;
+    }
+
 }
--- sdl_app/Particle.h
+++ sdl_app/Particle.h
@@ -11,6 +11,9 @@ public:
     double m_x;
     double m_y;
 
+    double m_xspeed;
+    double m_yspeed;
+
 
 public:
     Particle();
