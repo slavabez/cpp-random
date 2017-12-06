c318d64bc0cecde09dad12e900c0ca46c14dcd91
Slava Bezgachev <bezgachev@gmail.com>
2017-12-06 15:48:44 +0600

    vectors and iterating through them

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -3,64 +3,24 @@
 //
 
 #include <iostream>
-#include <fstream>
+#include <vector>
 
 using namespace std;
 
-
-#pragma pack(push, 1)
-
-struct Person {
-    char name[50];
-    int age;
-    double weight;
-};
-
-#pragma pack(pop)
-
 int main() {
 
-    Person someone = {"Frodo", 220, 65.5};
+    // Vectors are template classes
+    vector<string> strings;
 
-    string fileName = "test.bin";
+    strings.emplace_back("one");
+    strings.emplace_back("two");
+    strings.emplace_back("three");
 
-    /*
-     * Write binary file
-     */
 
-    ofstream outputFile;
 
-    outputFile.open(fileName, ios::binary);
-
-    if (outputFile.is_open()){
-
-        outputFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
-
-        outputFile.close();
-    } else {
-        cout << "Could not create file " << fileName << endl;
+    for (auto it = strings.begin(); it != strings.end(); it++){
+        cout << *it << ", size in bites: " << sizeof(*it)  << endl;
     }
-
-    /*
-     * Read binary file
-     */
-
-    Person someoneElse = {};
-
-    ifstream inputFile;
-
-    inputFile.open(fileName, ios::binary);
-
-    if (inputFile.is_open()){
-
-        inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
-
-        inputFile.close();
-    } else {
-        cout << "Could not create file " << fileName << endl;
-    }
-
-    cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.weight << endl;
 
 
     return 0;
